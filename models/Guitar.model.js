ed701655190be1d3bbfb68919fa49f821f5fa477
Ruben Andino <rubenanlo@gmail.com>
2022-07-15 18:27:51 +0200

    fixing typos

--- models/Guitar.model.js
+++ models/Guitar.model.js
@@ -1,42 +1,57 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
+const guitarSchema = new Schema(
   {
-    brand: String,
+    brand: {
+      type: String,
+      required: true,
+    },
 
-    model: String,
+    model: {
+      type: String,
+      required: true,
+    },
+
+    nickName: {
+      type: String,
+      required: true,
+    },
 
     type: {
       type: String,
-      enum: ['electric', 'classic', 'acoustic']
+      enum: ['electric', 'classic', 'acoustic'],
+      required: true,
     },
-    
+
     image: {
       type: String,
       default: '../public/images/guitar-default-img.png',
     },
-    
+
     fingerboardMaterial: String,
-    
-    artists:{
-      Type: [String]
+
+    artists: {
+      Type: [String],
     },
-    
+
     year: Number,
-    
+
     countryOrigin: String,
-    
+
     pickupConfig: String,
+
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
-);
+)
 
-const Guitar = model("Guitar", userSchema);
+const Guitar = model('Guitar', userSchema)
 
-module.exports = Guitar;
-
-
+module.exports = Guitar
