208c9af890fc2b368a9eafc45000da07a3df229f
Paul Tiry <tirypaul@gmail.com>
2022-07-18 20:09:41 +0200

    started profile routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,11 @@
-const router = require("express").Router();
+const Guitar = require('../models/Guitar.model')
+
+const router = require('express').Router()
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+router.get('/', async (req, res, next) => {
+  const guitarList = await Guitar.find()
+  res.render('guitars/guitars', { guitarList })
+})
 
-module.exports = router;
+module.exports = router
