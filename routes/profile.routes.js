b77cf70fd2c47fd085e75aae0ff9d0e18ecc9015
Paul Tiry <tirypaul@gmail.com>
2022-07-19 15:17:06 +0200

    new users functionality

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,17 +2,16 @@ const router = require('express').Router()
 
 const User = require('../models/User.model')
 const Guitar = require('../models/Guitar.model')
-
-const isLoggedOut = require('../middleware/isLoggedOut')
 const isLoggedIn = require('../middleware/isLoggedIn')
 
-router.get('/', isLoggedIn, async (req, res, next) => {
-  res.render('profile/profile-page', { user: req.session.user })
-})
-
 router.get('/:userId/collection', isLoggedIn, async (req, res, next) => {
-  const collection = await Guitar.find({ user: req.params.userId })
-  res.render('profile/collection', { collection })
+  try {
+    const collection = await Guitar.find({ user: req.params.userId })
+    const user = await User.findById(req.params.userId)
+    res.render('profile/my-collection', { collection, user })
+  } catch (err) {
+    console.log('Error finding your beaties: ', error)
+  }
 })
 
 module.exports = router
