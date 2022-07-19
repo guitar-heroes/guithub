87e83311e04d68f378659c49e207b15eb980073b
Paul Tiry <tirypaul@gmail.com>
2022-07-19 11:43:16 +0200

    new navbar

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,8 +4,7 @@ const router = require('express').Router()
 
 /* GET home page */
 router.get('/', async (req, res, next) => {
-  const guitarList = await Guitar.find()
-  res.render('guitars/guitars', { guitarList })
+  res.render('index')
 })
 
 module.exports = router
